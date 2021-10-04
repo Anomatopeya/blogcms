9d05ab01df72c5297d6e486b22692c8e19c67dd8
Anomatopeya <anomatopeya@gmail.com>
2021-10-04 13:16:27 +0300

    publish view

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # blog-crm
 `composer require aldwyn/blogcms:@dev`  
 `php artisan backpack:install`
-`php artisan vendor:publish --provider="Aldwyn\Blogcms\CmsServiceProvider" --tag="adminCrud"`
+`php artisan vendor:publish --provider="Aldwyn\Blogcms\CmsServiceProvider" --tag="cmsCustomView"`  
+`php artisan cms:publish {CrudName}`  
+
 
--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -21,10 +21,10 @@ class CmsServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes.php');
         $this->loadMigrationsFrom(__DIR__.'/migrations');
 
-        // Публикация Файлов Админ
+        // Публикация Вью Файлов Админ
         $this->publishes([
-            __DIR__.'/app/Http/Controllers/AdminPublishes' => app_path('Http/Controllers/Admin')
-        ], 'adminCrud');
+            __DIR__.'resources/views' => app_path('resources/views/vendor/backpack')
+        ], 'cmsCustomView');
 //        $this->loadViewsFrom(__DIR__.'/views', 'todolist');
 //        $this->publishes([
 //            __DIR__.'/views' => base_path('resources/views/wisdmlabs/todolist'),
