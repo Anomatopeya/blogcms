f4d1d64da393451a065abfa80ece3596e1ec3567
Anomatopeya <anomatopeya@gmail.com>
2021-10-04 12:07:29 +0300

    command for CRUD publish

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # blog-crm
 `composer require aldwyn/blogcms:@dev`  
 `php artisan backpack:install`
+`php artisan vendor:publish --provider="Aldwyn\Blogcms\CmsServiceProvider" --tag="adminCrud"`
 
---
+++ src/app/Console/Commands/PublishCrud.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Backpack\CRUD\app\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PublishCrud extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'cms:publish-crud';
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cms:publish-crud
+                            {crud : short crud name (ex: article)}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publishes a CMS admin CRUD so you can make changes to it, for your project.';
+
+    /**
+     * The directory where the views will be published FROM.
+     *
+     * @var string
+     */
+    public $sourcePath = 'vendor/aldwyn/blogcms/src/Http/Controllers/AdminPublishes';
+
+    /**
+     * The directory where the views will pe published TO.
+     *
+     * @var string
+     */
+    public $destinationPath = 'app/Http/Controllers/Admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->file = ucfirst(strtolower($this->argument('crud'))).'CrudController';
+
+        return $this->publishFile($this->file);
+    }
+
+    /**
+     * Take a blade file from the vendor folder and publish it to the resources folder.
+     *
+     * @param  string  $file  The filename without extension
+     * @return void
+     */
+    protected function publishFile($file)
+    {
+        $sourceFile = $this->sourcePath.$file.'.php';
+        $copiedFile = $this->destinationPath.$file.'.php';
+
+        if (! file_exists($sourceFile)) {
+            return $this->error(
+                'Cannot find source CRUD file at '
+                .$sourceFile.
+                ' - make sure you\'ve picked an existing CRUD file'
+            );
+        } else {
+            $canCopy = true;
+
+            if (file_exists($copiedFile)) {
+                $canCopy = $this->confirm(
+                    'File already exists at '
+                    .$copiedFile.
+                    ' - do you want to overwrite it?'
+                );
+            }
+
+            if ($canCopy) {
+                $path = pathinfo($copiedFile);
+
+                if (! file_exists($path['dirname'])) {
+                    mkdir($path['dirname'], 0755, true);
+                }
+
+                if (copy($sourceFile, $copiedFile)) {
+                    $this->info('Copied to '.$copiedFile);
+                } else {
+                    return $this->error(
+                        'Failed to copy '
+                        .$sourceFile.
+                        ' to '
+                        .$copiedFile.
+                        ' for unknown reason'
+                    );
+                }
+            }
+        }
+    }
+}
