eaef1e08166d31b7c1b6785c1d8503e0293538a9
Anomatopeya <anomatopeya@gmail.com>
2021-10-04 12:09:27 +0300

    command for CRUD publish

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -7,7 +7,8 @@ use Illuminate\Support\ServiceProvider;
 class CmsServiceProvider extends ServiceProvider
 {
     protected $commands = [
-        \Aldwyn\Blogcms\app\Console\Commands\AddSidebarContent::class
+        \Aldwyn\Blogcms\app\Console\Commands\AddSidebarContent::class,
+        \Aldwyn\Blogcms\app\Console\Commands\PublishCrud::class
     ];
     /**
     * Bootstrap the application services.
